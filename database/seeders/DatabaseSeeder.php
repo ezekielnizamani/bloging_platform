3d0bd484b733abf30bb1242da5f3b19aedf7d66b
ezekiel <ezekielnizamani0@gmail.com>
2023-11-05 13:36:25 +0500

    used pgsql and designed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +16,26 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' =>'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'role' => 0,
+            
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'hizqeel',
+            'email' =>'hizqeel@gmail.com',
+            'password' => Hash::make('hizqeel'),
+            'role' => 1,
+
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'john',
+            'email' =>'john@gmail.com',
+            'password' => Hash::make('john'),
+            'role' => 2,
+
+        ]);
     }
 }
